50a89d1aeb6d70d434e00be3548785960df1c380
Anthony Wilhelm <twilhelm@gmx.com>
2013-04-15 15:06:40 -0500

    git it started

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Reactor Child Theme Functions
+ *
+ * @package Reactor
+ * @author Anthony Wilhelm (@awshout / anthonywilhelm.com)
+ * @version 1.0.0
+ * @since 1.0.0
+ * @copyright Copyright (c) 2013, Anthony Wilhelm
+ * @license GNU General Public License v2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ */
+
+/* -------------------------------------------------------
+You can add your custom functions here
+-------------------------------------------------------- */
