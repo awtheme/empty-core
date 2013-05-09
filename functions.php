37e9258f4a4184aeae49ad2bf694b32230984f10
Anthony Wilhelm <twilhelm@gmx.com>
2013-05-08 19:54:33 -0500

    update to 1.1.0
    allow remove_theme_support for Reactor features

--- functions.php
+++ functions.php
@@ -4,12 +4,95 @@
  *
  * @package Reactor
  * @author Anthony Wilhelm (@awshout / anthonywilhelm.com)
- * @version 1.0.0
+ * @version 1.1.0
  * @since 1.0.0
  * @copyright Copyright (c) 2013, Anthony Wilhelm
  * @license GNU General Public License v2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
  */
 
 /* -------------------------------------------------------
-You can add your custom functions here
+ You can add your custom functions below
 -------------------------------------------------------- */
+
+
+/**
+ * Child Theme Features
+ * The following function will allow you to remove features included with Reactor
+ *
+ * Remove the comment slashes (//) next to the functions
+ * For add_theme_support, remove values from arrays to disable parts of the feature
+ * remove_theme_support will disable the feature entirely
+ * Reference the functions.php file in Reactor for add_theme_support functions
+ */
+add_action('after_setup_theme', 'reactor_child_theme_setup', 11);
+
+function reactor_child_theme_setup() {
+
+    /* Support for menus */
+	// remove_theme_support('reactor-menus');
+	// add_theme_support(
+	// 	'reactor-menus',
+	// 	array('top-bar-l', 'top-bar-r', 'main-menu', 'side-menu', 'footer-links')
+	// );
+	
+	/* Support for sidebars
+	Note: this doesn't change layout options */
+	// remove_theme_support('reactor-sidebars');
+	// add_theme_support(
+	// 	'reactor-sidebars',
+	// 	array('primary', 'secondary', 'front-primary', 'front-secondary', 'footer')
+	// );
+	
+	/* Support for layouts
+	Note: this doesn't remove sidebars */
+	// remove_theme_support('reactor-layouts');
+	// add_theme_support(
+	// 	'reactor-layouts',
+	// 	array('1c', '2c-l', '2c-r', '3c-l', '3c-r', '3c-c')
+	// );
+	
+	/* Support for custom post types */
+	// remove_theme_support('reactor-post-types');
+	// add_theme_support(
+	// 	'reactor-post-types',
+	// 	array('slides', 'portfolio')
+	// );
+	
+	/* Support for page templates */
+	// remove_theme_support('reactor-page-templates');
+	// add_theme_support(
+	// 	'reactor-page-templates',
+	// 	array('front-page', 'news-page', 'portfolio', 'contact')
+	// );
+	
+	/* Remove support for background options in customizer */
+	// remove_theme_support('reactor-backgrounds');
+	
+	/* Remove support for font options in customizer */
+	// remove_theme_support('reactor-fonts');
+	
+	/* Remove support for custom login options in customizer */
+	// remove_theme_support('reactor-custom-login');
+	
+	/* Remove support for breadcrumbs function */
+	// remove_theme_support('reactor-breadcrumbs');
+	
+	/* Remove support for page links function */
+	// remove_theme_support('reactor-page-links');
+	
+	/* Remove support for page meta function */
+	// remove_theme_support('reactor-post-meta');
+	
+	/* Remove support for taxonomy subnav function */
+	// remove_theme_support('reactor-taxonomy-subnav');
+	
+	/* Remove support for shortcodes */
+	// remove_theme_support('reactor-shortcodes');
+	
+	/* Remove support for tumblog icons */
+	// remove_theme_support('reactor-tumblog-icons');
+	
+	/* Remove support for other langauges */
+	// remove_theme_support('reactor-translation');
+		
+}
